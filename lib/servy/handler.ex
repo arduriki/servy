47df53240e8aa61fab2844dbcc5e6e65913bee48
Jordi Ardura <jordi.ardura.professional@gmail.com>
2025-08-22 06:56:57 +0000

    first commit

---
+++ lib/servy/handler.ex
@@ -0,0 +1,26 @@
+defmodule Servy.Handler do
+    def handle(request) do
+
+    end
+end
+
+# HTTP request with multiplelines string
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
