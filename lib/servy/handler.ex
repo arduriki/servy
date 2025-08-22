b939c48fe8b1a10958bd68f326bf82b6f936e889
Jordi Ardura <jordi.ardura.professional@gmail.com>
2025-08-22 10:04:16 +0000

    parse and route functions updated

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,7 +1,40 @@
 defmodule Servy.Handler do
-    def handle(request) do
+  def handle(request) do
+    request
+    |> parse
+    |> route
+    |> format_response
+  end
 
-    end
+  def parse(request) do
+    # Create a map with the method and the path
+    [method, path, _] =
+      request
+      # Split request into a list of lines.
+      |> String.split("\n")
+      # Get the first line.
+      |> List.first()
+      # Split the first line to three parts.
+      |> String.split(" ")
+
+    # The last expression is the one that is returned from the function
+    %{method: method, path: path, resp_body: ""}
+  end
+
+  def route(conv) do
+    # Update the map (a copy of it rebinded to conv) with a response body.
+    %{conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: #{String.length(conv.resp_body)}
+
+    #{conv.resp_body}
+    """
+  end
 end
 
 # HTTP request with multiplelines string
@@ -11,14 +44,6 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
-"""
-
-expected_response = """
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: 20
-
-Bears, Lions, Tigers
 """
 
 response = Servy.Handler.handle(request)
